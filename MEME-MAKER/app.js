5631253c49276f8cbe011270c1226b4926bb9891
aranchini <s26032@gsm.hs.kr>
2026-08-26 18:29:19 +0900

    Modebtn 추가

--- MEME-MAKER/app.js
+++ MEME-MAKER/app.js
@@ -1,14 +1,75 @@
+const modeBtn = document.getElementById("mode-btn");
+const colorOptions = Array.from(document.getElementsByClassName("color-option"));
+const color = document.getElementById("color");
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = lineWidth.value;
+let isPainting = false;
+let isFilling = false;
 
-ctx.fillRect(200,200,50,200);
-ctx.fillRect(400,200,50,200);
-ctx.lineWidth =2;
-ctx.strokeRect(300,300,50,100);
-ctx.fillRect(200,200,200,20);
-ctx.moveTo(200,200);
-ctx.lineTo(325,100);
-ctx.lineTo(450,200);
-ctx.fill();
+
+function onMove(event) {
+    if(isPainting){
+        ctx.lineTo(event.offsetX, event.offsetY);
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+}
+
+
+function onMousedown(event) {
+    isPainting = true;
+}
+
+function onMouseup(event) {
+    isPainting = false;
+    ctx.beginPath();
+}
+
+function onlineWidthChange(event) {
+    ctx.lineWidth = event.target.value;
+}
+
+function onColorChange(event) {
+    ctx.strokeStyle = event.target.value;
+    ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event) {
+    const colorValue = event.target.dataset.color;
+    ctx.strokeStyle = colorValue;
+    ctx.fillStyle = colorValue;
+    color.value = colorValue;
+}
+
+function onModeClick(event) {
+    if(isFilling){
+        isFilling = false;
+        modeBtn.innerText = "Fill";
+    } else {
+        isFilling = true;
+        modeBtn.innerText = "Draw";
+    }
+}
+
+function onCanvasClick() {
+    if(isFilling){
+        ctx.fillRect(0, 0, 800, 800);
+    }
+}
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", onMousedown);
+canvas.addEventListener("mouseup", onMouseup);
+canvas.addEventListener("mouseleave", onMouseup);
+canvas.addEventListener("click", onCanvasClick);
+
+lineWidth.addEventListener("change", onlineWidthChange);
+color.addEventListener("change", onColorChange);
+
+colorOptions.forEach(color => color.addEventListener("click", onColorClick));
+
+modeBtn.addEventListener("click", onModeClick);
